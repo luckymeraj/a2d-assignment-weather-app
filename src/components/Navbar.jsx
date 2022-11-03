9eb2203c75afbe7477920a4c976cc40d1f368983
Meraj Rizwan <meraj.rizwan9832@gmail.com>
2022-11-03 06:45:19 +0530

    bg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,6 @@ const Navbar = ({ findCity }) => {
                     </button>
                     <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                         <div className="offcanvas-header">
-                            {/* <h5 className="offcanvas-title" id="offcanvasNavbarLabel">A2D Forcasting</h5> */}
 
                             <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
--- src/pages/Wdashboard.jsx
+++ src/pages/Wdashboard.jsx
@@ -6,10 +6,20 @@ import { getWeatherData } from '../WeatherService'
 const Wdashboard = () => {
   const [weather, setWeather] = useState(null)
   const [city,findCity]=useState('asansol')
+  const [bg,setBg]=useState('https://wallpapercave.com/wp/wp7260934.jpg')
   const fetchWeatherData = async () => {
     const data = await getWeatherData(city)
     // console.log(data);
     setWeather(data)
+    if(data.temp>=25){
+      setBg('https://wallpapercave.com/wp/wp7260934.jpg')
+    }
+    else if(data.temp<25&&data.temp>15){
+      setBg('https://media1.popsugar-assets.com/files/thumbor/n8bUt1yPUD0zvO0XmWNFuVj763k/685x0:3345x2660/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/01/07/726/n/1922441/419ff9b05e14b139162ef6.64727550_/i/Best-Winter-Photos.jpg')
+    }
+    else if(data.temp<=15){
+      setBg('https://images2.minutemediacdn.com/image/upload/c_fill,w_1440,ar_16:9,f_auto,q_auto,g_auto/shape/cover/sport/111798556-565x376-1-5102d44ede42632f8b80b83ccde7e7a1.jpg')
+    }
   }
   useEffect(() => {
    
@@ -24,11 +34,11 @@ const Wdashboard = () => {
   
       {
         weather && (
-          <div className="main-container">
+          <div className="main-container" style={{backgroundImage:`url(${bg})`}}>
         
 
 
-        <div className="city ">
+        <div className="city vert-move">
           <h2 className="city-name">
             <span>{`${weather.name}`}</span>
             <sup>{`${weather.country}`}</sup>
